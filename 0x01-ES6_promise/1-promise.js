71d60fa704327703f704035b8214b6fc863ab806
JosepharDev <josephardev@gmail.com>
2024-10-02 17:11:35 +0000

    add

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,13 @@
-export default function getFullResponseFromAPI(success){
-  return new Promise((resolve,reject) => {
-    if (success){
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
       resolve({
         status: 200,
-	body: ''Success,
+        body: 'Success',
       });
-    } else{
+    } else {
       reject(new Error(
-      		'The fake API is not working currently',
+        'The fake API is not working currently',
       ));
     }
   });
